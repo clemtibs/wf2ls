3fb5b2249d6fa1daab501e6e535a8f8b6421b2fa
Brian Clements-Tiberio <brian@brianclements.net>
2024-11-26 10:38:39 -0800

    refactor: rename time.js -> date.js, move to src folder

--- src/wf2ls.js
+++ src/wf2ls.js
@@ -1,4 +1,4 @@
-const time = require('./time.js');
+const date = require('./date.js');
 const progress = require('./progress.js');
 
 let argv = require('minimist')(process.argv.slice(2));
@@ -32,11 +32,11 @@ const processNode = (node) => {
   // if (!resultIdMap.get(node.id)) resultIdMap.set(node.id, result);
   result.name = node.nm;
   // result.id = node.id;
-  // if (node.ct) result.created = time.wfTimeToLocalTime(node.ct, time.wfEpochSecondsPst);
+  // if (node.ct) result.created = date.wfTimeToLocalTime(node.ct, date.wfEpochSecondsPst);
   if (node.no) result.note = node.no;
-  if (node.cp) result.completed = time.wfTimeToLocalTime(node.cp, time.wfEpochSecondsPst);
+  if (node.cp) result.completed = date.wfTimeToLocalTime(node.cp, date.wfEpochSecondsPst);
   if (node.metadata.layoutMode == "todo") result.layoutMode = "todo";
-  // result.lastModified = time.wfTimeToLocalTime(node.lm, time.wfEpochSecondsPst);
+  // result.lastModified = date.wfTimeToLocalTime(node.lm, date.wfEpochSecondsPst);
   // node.mirrorRootItems?.forEach(item => mirrors.set(item.id, node.id));
   if (node.ch) result.children = node.ch.map(child => processNode(child));
   return result;
