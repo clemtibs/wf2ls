d7b755b2c3306bdd75824b2485b4261eb303f09b
Brian Clements-Tiberio <brian@brianclements.net>
2024-12-02 16:07:04 -0800

    test(date,node): add pending tests

---
+++ test/date.test.js
@@ -0,0 +1,9 @@
+// const { expect } = import('chai');
+
+describe('date.js', () => {
+  describe('Time Conversions', () => {
+    describe('wfTimeToLocalTime', () => {
+      it('Converts time to default format');
+    });
+  });
+});
---
+++ test/node.test.js
@@ -0,0 +1,22 @@
+// const { expect } = import('chai');
+// import { node } from '../src/node.js';
+
+describe('node.js', () => {
+  describe('Node Processing', () => {
+    describe('processNote()', () => {
+      it('Creates indent equal to default for each line of text when indentTxt is 0');
+      it('Creates default x n spaces for each line of text when indentTxt is n');
+    });
+  });
+
+  describe('Idendity Tests', () => {
+    describe('nodeIsBacklink()', () => {
+      it('should detect backlink nodes')
+        // , () => {
+        // let testNode = { 'metadata':
+                      // {'isReferencesRoot': true }}
+        // expect(node.nodeIsBacklink(testNode)).to.be.true;
+      // });
+    });
+  });
+});
