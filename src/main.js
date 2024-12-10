15df739962d7aa258db9ae70b1dab3f4d51ee4a2
Brian Clements-Tiberio <brian@brianclements.net>
2024-12-10 12:04:51 -0800

    refactor(wf2ls, main): make main() fully independent of CLI mode
    main() now just takes state, config, and data and does it's work to
    state directly. wf2ls handles CLI args and writing to file. main() now
    could potentially be used as a library for a web app.

--- src/main.js
+++ src/main.js
@@ -1,21 +1,11 @@
-import { loadSrcFile, writeFile } from './fs.js';
 import { parse2md } from './md.js';
 import { parseData } from './processing.js';
 
-const main = (state, conf) => {
-  const rawData = loadSrcFile(conf.sourceFile);
-  const parsedData = parseData(state, rawData);
+const main = (state, conf, data) => {
+  const parsedData = parseData(state, data);
   state.startProgressBar();
   parse2md(state, conf, conf.defaultPage, parsedData, parsedData.length);
   state.stopProgressBar();
-
-  if (!state.isTestInstance) {
-    for (let [page, content] of state.pages) {
-      writeFile(content, page + ".md", conf.destDir);
-    }
-  } else {
-    return state
-  }
 }
 
 export { main };
--- src/wf2ls.js
+++ src/wf2ls.js
@@ -4,8 +4,15 @@ import { AppState } from './state.js';
 import { appConfig, loadArgsToConfig } from './config.js';
 import { mainProgressBar } from './progress.js';
 import { main } from './main.js';
+import { loadSrcFile, writeFile } from './fs.js';
 
+// CLI entrypoint for main()
 const mainState = new AppState(mainProgressBar);
 const mainConfig = loadArgsToConfig(appConfig, minimist(process.argv.slice(2)));
+const rawData = loadSrcFile(mainConfig.sourceFile);
 
-main(mainState, mainConfig);
+main(mainState, mainConfig, rawData);
+
+for (let [page, content] of mainState.pages) {
+  writeFile(content, page + ".md", mainConfig.destDir);
+}
