8f0d41f25b45c3f5dd014ecee98ed55f6a6949a1
Brian Clements-Tiberio <brian@brianclements.net>
2024-12-10 12:15:50 -0800

    test(state.test): add AppState object tests

---
+++ test/state.test.js
@@ -0,0 +1,51 @@
+import { expect } from 'chai';
+
+import { AppState } from '../src/state.js';
+import { appConfig } from '../src/config.js';
+import { main } from '../src/main.js';
+import { loadSrcFile } from '../src/fs.js';
+
+const largeSampleDataLoc = "./test/data/wf_data_sample.json";
+
+describe('state.js', () => {
+  describe('AppState instances', () => {
+    const testState = new AppState(undefined, true);
+    it('Has required properties', () => {
+      expect(testState).to.have.all.keys('pages', 'mirrors', 'isTestInstance');
+      expect(testState.pages).to.be.an.instanceOf(Map);
+      expect(testState.mirrors).to.be.an.instanceOf(Map);
+      expect(testState.addJob).to.be.an.instanceOf(Function);
+      expect(testState.addPage).to.be.an.instanceOf(Function);
+      expect(testState.incrementJobProgress).to.be.an.instanceOf(Function);
+      expect(testState.startProgressBar).to.be.an.instanceOf(Function);
+      expect(testState.stopProgressBar).to.be.an.instanceOf(Function);
+    });
+    it('Does not show internal properties', () => {
+      expect(testState).to.not.have.all.keys('totalNumJobs', 'jobProgress', 'progressBar');
+    });
+    describe('Console output', () => {
+      const testState = new AppState(undefined, true);
+      it('Does not call progressBar methods' , () => {
+        expect(testState.addJob()).to.be.undefined;
+        expect(testState.startProgressBar()).to.be.undefined;
+        expect(testState.incrementJobProgress()).to.be.undefined;
+        expect(testState.stopProgressBar()).to.be.undefined;
+      });
+    });
+    it('Returns state object when testing', () => {
+      const testState = new AppState(undefined, true);
+      // shallow copy not good enough, need to make config class
+      // const testConfig = { ...appConfig };
+      const testConfig = {
+        sourceFile: largeSampleDataLoc,
+        destDir: "",
+        newPageTag: "#LS-Page",
+        indentSpaces: 2,
+        defaultPage: "Page One"
+      };
+      const testData = loadSrcFile(testConfig.sourceFile);
+      const results = main(testState, testConfig, testData);      
+      expect(results).to.be.an.instanceOf(AppState);
+    });
+  });
+});
